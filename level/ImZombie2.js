06ce53e703f9e9baa775ea05f2bc15214411acaf
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-09 18:59:44 +0800

    Update ImZombie2.js

--- level/ImZombie2.js
+++ level/ImZombie2.js
@@ -1,6 +1,6 @@
 oS.Init({
 PName:[oPeashooter,oSunFlower,oSnowPea,oSpikeweed],
-ZName:[oZombie,oScreenDoorZombie,oConeheadZombie],
+ZName:[oZombie,oScreenDoorZombie,oConeheadZombie,oJackinTheBoxZombie],
 PicArr:["images/interface/background2.jpg","images/interface/trophy.png","images/interface/Stripe.png"],
 backgroundImage:"images/interface/background2.jpg",
 ShowScroll:false,
