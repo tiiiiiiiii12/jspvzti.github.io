5ee1062cc66d5349ad5bfde1494f4a79e3ea8821
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-09 10:31:26 +0800

    Update CPlant.js

--- js/CPlant.js
+++ js/CPlant.js
@@ -660,7 +660,7 @@ var CPlants = NewO({
         AudioArr: ["splat1", "splat2", "splat3", "plastichit", "shieldhit", "shieldhit2"],
         Tooltip: "一次发射四颗豌豆<br>(需要双发射手)",
         Produce: '机枪射手可以一次发射四颗豌豆<p>伤害：<font color="#FF0000">中等(每颗)</font><br>发射速度：<font color="#FF0000">四倍<br>只能种在双发射手上</font></p>当机枪豌豆宣布他要参军的时候，他的父母很为他担心，他们异口同声地对他说：“亲爱的，但这太危险了。”加特林拒绝让步，“生活本就危险，”他这样回答着，此时他的眼睛里，正闪烁着钢铁般的信念。',
-        PrivateBirth: this.jinyin?function(c) {
+        PrivateBirth: this.jinyin?oThreepeater.prototype.PrivateBirth:function(c) {
             var b = c.AttackedLX,
                 a = b - 40;
             c.BulletClass = NewO({
@@ -674,7 +674,7 @@ var CPlants = NewO({
                 F: oGd.MB1
             });
             c.BulletEle = NewImg(0, "images/Plants/PB00.gif", "left:" + a + "px;top:" + (c.pixelTop + 8) + "px;visibility:hidden;z-index:" + (c.zIndex + 2))
-        }:oThreepeater.prototype.PrivateBirth,
+        },
         CanGrow: function(b, a, d) {
             var c = b[1];
             return c && c.EName == "oRepeater"
