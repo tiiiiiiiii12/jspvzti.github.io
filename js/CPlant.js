0aa1499c2b315c1bdcd3a6c9537e5bc90b63630e
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-09 12:41:02 +0800

    Update CPlant.js

--- js/CPlant.js
+++ js/CPlant.js
@@ -685,8 +685,7 @@ var CPlants = NewO({
             return c && c.EName == "oRepeater"
         },
         getTriggerR:function(a){
-        var b=this;
-        return (b.jinyin?[a > 2 ? a - 1 : 1, a < oS.R ? Number(a) + 1 : a]:[a, a])
+        return [a > 2 ? a - 1 : 1, a < oS.R ? Number(a) + 1 : a]
         },
         jinyinAct:function(a){
         a.NormalAttack1=oThreepeater.prototype.NormalAttack;
