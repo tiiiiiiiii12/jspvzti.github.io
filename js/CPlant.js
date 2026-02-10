1fa141deb4b5fbb52a9a4a516da70d19ce909980
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-10 11:46:34 +0800

    Update CPlant.js

--- js/CPlant.js
+++ js/CPlant.js
@@ -2670,9 +2670,9 @@ var CPlants = NewO({
                     var j, f = GetC(k),
                         e = oZ["getZ" + d](k, h);
                 if(num>75){
-                    e && e.Altitude == 1&&(e.getPea(e, 2, d)),(k += (j = !d ? 5 : -5)) < oS.W && k > 100 ? (i.style.left = (l += j) + "px", oSym.addTask(1, arguments.callee, [g, i, d, k, h, l])) : ClearChild(i);
+                    e && e.Altitude == 1&&(e.getHit0(e, 2, d)),(k += (j = !d ? 5 : -5)) < oS.W && k > 100 ? (i.style.left = (l += j) + "px", oSym.addTask(1, arguments.callee, [g, i, d, k, h, l])) : ClearChild(i);
                 }else{
-                e && e.Altitude == 1?(e.num>50&&e.getr(e,20),e.getPea(e,20,d), ClearChild(i)):(k += (j = !d ? 5 : -5)) < oS.W && k > 100 ? (i.style.left = (l += j) + "px", oSym.addTask(1, arguments.callee, [g, i, d, k, h, l])) : ClearChild(i);
+                e && e.Altitude == 1?(num>50&&e.getr(e,20),e.getPea(e,20,d), ClearChild(i)):(k += (j = !d ? 5 : -5)) < oS.W && k > 100 ? (i.style.left = (l += j) + "px", oSym.addTask(1, arguments.callee, [g, i, d, k, h, l])) : ClearChild(i);
                 }
                 },
                 [c, $(c), 0, b.AttackedLX, b.R, b.AttackedLX - 40])
