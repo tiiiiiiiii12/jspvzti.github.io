a39b639198a7d5d00033ce52048e6681799e175a
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-22 10:59:58 +0800

    Update ImZombiePool1.js

--- level/ImZombiePool1.js
+++ level/ImZombiePool1.js
@@ -3,7 +3,7 @@
         关卡植物: 5大喷菇 3南瓜头 3魅惑菇 4土豆地雷 8向日葵 4三线射手 3机枪射手 3仙人掌 10荷叶
 */
 oS.Init({
-    PName: [oFumeShroom, oPumpkinHead, oHypnoShroom, oPotatoMine, oSunFlower, oThreepeater, oGatlingPea, oCactus, oPeashooter, oRepeater, oJalapeno, oGarlic, oPlantern],
+    PName: [oFumeShroom, oPumpkinHead, oHypnoShroom, oPotatoMine, oSunFlower, oThreepeater, oGatlingPea, oCactus, oPeashooter, oRepeater, oJalapeno, oGarlic, oPlantern,oIceShroom],
     ZName: [oZombie2, oBucketheadZombie, oDuckyTubeZombie2, oDuckyTubeZombie3, oNewspaperZombie, oBalloonZombie,oDolphinRiderZombie,oFlagZombie,oPoleVaultingZombie],
     PicArr: ["images/interface/background4.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
     backgroundImage: "images/interface/background4.jpg",
@@ -24,7 +24,7 @@ oS.Init({
         ArR: [1, 6],
         Auto: 1,
         P: {
-            Arr: [8, 4, 0, 0, 0, 11, 12, 10, 5, 2, 1, 2, 4, 9, 4, 4, 4, 5, 6, 10, 9, 8, 7, 7, 8],
+            Arr: [8, 4, 0, 0, 0, 11, 12, 13, 5, 2, 1, 2, 4, 9, 4, 4, 4, 5, 6, 10, 9, 8, 7, 7, 8],
             Arr1: [3, 3, 3, 3],
             Arr2: [1, 1, 1]
         }
@@ -62,6 +62,7 @@ oS.Init({
         oS.RiddleAutoGrow();
     }
 });
+
 
 
 
