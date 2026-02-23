04c6a1f336b84061d02b5672748ffcf7713f7c25
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-23 22:54:58 +0800

    Update CPlant.js

--- js/CPlant.js
+++ js/CPlant.js
@@ -1092,14 +1092,14 @@ NormalAttack1: function() {
         height: 67,
         beAttackedPointL: 15,
         beAttackedPointR: 82,
-        SunNum: 125,
+        SunNum: 200,
         PKind: 2,
         HP: 4000,
         coolTime: 30,
         zIndex: 1,
         PicArr: ["images/Card/Plants/PumpkinHead.png", "images/Plants/PumpkinHead/0.gif", "images/Plants/PumpkinHead/PumpkinHead.gif", "images/Plants/PumpkinHead/PumpkinHead1.gif", "images/Plants/PumpkinHead/PumpkinHead2.gif", "images/Plants/PumpkinHead/pumpkin_damage1.gif", "images/Plants/PumpkinHead/pumpkin_damage2.gif", "images/Plants/PumpkinHead/Pumpkin_back.gif"],
         Tooltip: "能保护种在里面的植物",
-        Produce: '南瓜头，可以用他的外壳保护其他植物。<p>韧性：<font color="#FF0000">高</font><br>特点：<font color="#FF0000">可以种在其他植物上</font></p>南瓜头最近都没收到，关于他表哥刃菲尔德的消息。很明显，刃菲尔德是个大明星，是一种……叫什么运动来着……的体育明星？佩格跳跳球大师？南瓜头反正搞不懂是什么运动，他只想做好他自己的工作。',
+        Produce: '南瓜头，可以用他的外壳保护其他植物，濒死或被碾压直接爆炸<p>韧性：<font color="#FF0000">高</font><br>特点：<font color="#FF0000">可以种在其他植物上</font></p>南瓜头最近都没收到，关于他表哥刃菲尔德的消息。很明显，刃菲尔德是个大明星，是一种……叫什么运动来着……的体育明星？佩格跳跳球大师？南瓜头反正搞不懂是什么运动，他只想做好他自己的工作。',
         CanGrow: function(c, b, d) {
             var a = b + "_" + d;
             return c[2] ? 1 : oGd.$LF[b] == 1 ? !(d < 1 || d > 9 || oGd.$Crater[a] || oGd.$Tombstones[a]==1) : c[0]
@@ -1108,16 +1108,19 @@ NormalAttack1: function() {
             return a[0] ? -12 : -5
         },
         HurtStatus: 0,
+		NormalAttack:oCherryBomb.prototype.PrivateBirth,
         getHurt: function(e, c, b) {
             var d = this,
                 f = d.id,
                 a = $(f);
             switch (true) {
                 case c && c < 3:
-                    d.Die();
+					d.getHurt=function(){};
+                    d.NormalAttack(d,0);
                     break;
                 case (d.HP -= b) < 1:
-                    d.Die();
+					d.getHurt=function(){};
+                    d.NormalAttack(d,0);
                     break;
                 case d.HP < 1334:
                     d.HurtStatus < 2 && (d.HurtStatus = 2, a.childNodes[1].src = "images/Plants/PumpkinHead/pumpkin_damage2.gif");
@@ -1587,7 +1590,7 @@ NormalAttack1: function() {
                         } while (g++ < e);
                         c.Die(1);
                         EditEle(f.childNodes[1], {
-                            src: c.PicArr[3] + Math.random()
+                            src:oCherryBomb.prototype.PicArr[3]+ Math.random()
                         }, {
                             width: "213px",
                             height: "196px",
