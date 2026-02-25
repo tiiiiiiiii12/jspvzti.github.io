405170c525d97a66f79bdf7e10b4f7930c0dc349
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-25 16:37:21 +0800

    Create IZtest.js

---
+++ level/IZtest.js
@@ -0,0 +1,70 @@
+oS.Init({
+    PName: [],
+    ZName: [oImp,oZombie,oConeheadZombie,oBucketheadZombie,oFlagZombie,oScreenDoorZombie, oDuckyTubeZombie1,oDuckyTubeZombie2,
+oPoleVaultingZombie,oDuckyTubeZombie3, oNewspaperZombie, oDolphinRiderZombie, oPoleVaultingZombie,
+oGargantuar, oSnorkelZombie,oFootballZombie,oDancingZombie,oZomboni,oJackinTheBoxZombie,oBalloonZombie,oBackupDancer],
+    PicArr: ["images/interface/background4.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
+    backgroundImage: "images/interface/background4.jpg",
+    Coord: 2,
+    DKind: 0,
+    LF: [0, 1, 1, 2, 2, 1, 1],
+    ShowScroll: false,
+    ProduceSun: false,
+    SunNum: 114514,
+    BrainsNum: 6,
+    CardKind: 1,
+    LevelName: "斗蛐蛐测试页面",
+    LvlEName: "ImZombieTest",
+    LoadMusic: "Mountains",
+    StartGameMusic: "Mountains",
+    LoadAccess: function(i) {
+        !oS.LvlVar ? oS.LvlVar = {
+            ScrollScreen: oS.ScrollScreen
+        } : oS.LvlVar.ScrollScreen = oS.ScrollScreen;
+        oS.RiddleAutoGrow();
+        $("tGround").style.left = "-115px";
+        SetStyle($("dTop"), {
+            left: "105px",
+            top: 0,
+            visibility: "visible"
+        });
+        innerText(ESSunNum, oS.SunNum);
+        oS.ScrollScreen = function() {
+            $("tGround").style.left = 0;
+            ClearChild($("dButton1"), $("dButton2"));
+            (function() {
+                (EDAll.scrollLeft += 25) < 500 ? oSym.addTask(2, arguments.callee, []) : SetVisible($("dMenu"), $("dSelectCard"), $("dCardList"));
+            })();
+        };
+        NewEle("dButton1", "button", "position:absolute;left:650px;top:510px;width:100px;height:35px;z-index:255", {
+            innerHTML: "开始挑战",
+            onclick: function() {
+                i(0);
+            }
+        }, EDAll);
+        NewEle("dButton2", "button", "position:absolute;left:760px;top:510px;width:100px;height:35px;z-index:255", {
+            innerHTML: "不玩这个",
+            onclick: function() {
+                SelectModal(0);
+            }
+        }, EDAll);
+    },
+    RiddleAutoGrow: function() {
+        for (var i = j[0]; i <= j[1]; ++i) CustomSpecial(oBrains, i, 0); // 脑子
+    },
+    StartGame: function() {
+        NewEle("dButton", "button", "position:absolute;left:150px;top:150px;width:100px;height:35px;z-index:255", {
+            innerHTML: "全体魅惑",
+            onclick: function() {
+             var i=1;
+             while(i++<oS.R){
+                var b=oZ.getArZ(0,oS.W).length;
+                b&&b.bedevil(b);
+               }
+            }
+        }, EDAll);
+        oP.Monitor(), BeginCool();
+        SetVisible($("dFlagMeter"), $("dTop"));
+        oS.RiddleAutoGrow();
+    }
+});
