18e07557657e1f8b705beba0b0d876a4ee8718d7
jspvztiiiiiiiii <1825121802@qq.com>
2026-02-26 18:33:07 +0800

    Update IZtest.js

--- level/IZtest.js
+++ level/IZtest.js
@@ -89,11 +89,11 @@ oGargantuar, oSnorkelZombie,oFootballZombie,oDancingZombie,oZomboni,oJackinTheBo
                 do {
                     k = (n = oZ.getArZ(0,100, h)).length;
                         while (k--) {
-                            n[k].PZ&&n[k].getr(n[k],800)
+                           n[k].getr(n[k],800)
                         }
-                     c = (m = oZ.getArZ(780,990, h)).length;
+                     c = (m = oZ.getArHZ(780,990, h)).length;
                         while (c--) {
-                            !m[c].PZ&&m[c].getr(m[c],-800)
+                            m[c].getr(m[c],-800)
                         }
                    } while (h++ < oS.R)
             oSym.addTask(1,arguments.callee,[])
