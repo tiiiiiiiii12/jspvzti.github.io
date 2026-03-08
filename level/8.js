1c384644c80bb9dc9651d74b493d064bc9d5351d
jspvztiiiiiiiii <1825121802@qq.com>
2026-03-08 08:39:27 +0800

    Update 8.js

--- level/8.js
+++ level/8.js
@@ -1,6 +1,6 @@
 oS.Init({
 	PName: [oPeashooter, oSunFlower, oCherryBomb, oWallNut, oPotatoMine, oSnowPea, oChomper],
-	ZName: [oZombie, oZombie2, oZombie3, oConeheadZombie, oBucketheadZombie],
+	ZName: [oZombie, oZombie2, oZombie3, oConeheadZombie, oBucketheadZombie,oImp],
 	PicArr: function() {
 		var a = oRepeater.prototype,
 			b = a.PicArr;
@@ -11,7 +11,7 @@ oS.Init({
 	LevelName: "关卡 1-8",
 	LvlEName: 8,
 	LargeWaveFlag: {
-		10: $("imgFlag1")
+		12: $("imgFlag1")
 	}
 }, {
 	AZ: [
@@ -19,15 +19,16 @@ oS.Init({
 		[oZombie2, 2, 1],
 		[oZombie3, 2, 1],
 		[oConeheadZombie, 2, 1],
-		[oBucketheadZombie, 1, 1]
+		[oBucketheadZombie, 1, 1],
+		[oImp, 1, 1]
 	],
-	FlagNum: 10,
+	FlagNum: 12,
 	FlagToSumNum: {
-		a1: [3, 5, 9],
-		a2: [1, 3, 5, 15]
+		a1: [3, 5, 11],
+		a2: [1, 5, 10, 30]
 	},
 	FlagToMonitor: {
-		9: [ShowFinalWave, 0]
+		11: [ShowFinalWave, 0]
 	},
 	FlagToEnd: function() {
 		NewImg("imgSF", "images/Card/Plants/Repeater.png", "left:827px;top:525px;clip:rect(auto,auto,60px,auto)", EDAll, {
