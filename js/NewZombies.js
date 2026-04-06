9dbcbbb6f24d64b4421369a87bd27cd57cf8e394
jspvztiiiiiiiii <1825121802@qq.com>
2026-04-06 08:35:51 +0800

    Update NewZombies.js

--- js/NewZombies.js
+++ js/NewZombies.js
@@ -43,7 +43,7 @@ var oGargantuar = InheritO(oZombie, {
           g;
         h && !h.FreeFreezeTime && !h.FreeSetbodyTime && ((g = $Z[e]) && g.getHit0(g, 1000, 0),
           oSym.addTask(25, function(h) {
-            h && h.JudgeAttack()
+            $Z[f] && h.JudgeAttack()
           }, [h]))
       },
       [d, c])
@@ -125,7 +125,7 @@ var oGargantuar = InheritO(oZombie, {
       for (i = -1; i <= 3; i++) {
         h && h.beAttacked && !h.FreeFreezeTime && !h.FreeSetbodyTime && ((d = $P[e]) && (tp = oGd.$[d.R + "_" + d.C + "_" + i]) && tp.getHurt(h, 1, 50),
           oSym.addTask(25, function(h) {
-            h && h.JudgeAttack()
+            $Z[f] && h.JudgeAttack()
           }, [h]))
       }
     }, [d, c]);
@@ -148,7 +148,7 @@ var oGargantuar = InheritO(oZombie, {
       top: "100px",
       clip: "rect(0,auto,200px,0)"
     }), h.intowater = true, 
-	$(h.Ele.FumeDoor)&&$(h.Ele.FumeDoor).style.left=(ImgLeft+200)+"px",
+	$(h.Ele.FumeDoor)&&($(h.Ele.FumeDoor).style.left=(ImgLeft+200)+"px"),
 	SetHidden(h.EleShadow), NewEle(a = h.id + "_splash", "div", "position:absolute;background:url(images/interface/splash.png);left:126px;top:" + (h.height - 88) + "px;width:97px;height:88px;over-flow:hidden", 0, h.Ele), ImgSpriter(a, h.id, [
         ["0 0", 9, 1],
         ["-97px 0", 9, 2],
@@ -165,7 +165,7 @@ var oGargantuar = InheritO(oZombie, {
     h.intowater && (oGd.$LF[h.R] == 2) && (h.ZX > GetX(9) || h.ZX < GetX(0)) && (SetStyle(h.EleBody, {
       top: "0px",
       clip: "rect(0,auto,300px,0)"
-    }, SetVisible(h.EleShadow)),$(h.Ele.FumeDoor)&&$(h.Ele.FumeDoor).style.left=(ImgLeft-200)+"px", h.intowater = false);
+    }, SetVisible(h.EleShadow)),$(h.Ele.FumeDoor)&&($(h.Ele.FumeDoor).style.left=(ImgLeft-200)+"px"), h.intowater = false);
     if (h.jinyin) {
       var P = $(h.id);
       !h.hasthrew&&!(h.PZ == h.check) && (
