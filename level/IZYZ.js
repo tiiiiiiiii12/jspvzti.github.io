34e49a76408bfae31c1256cf7d737aadb508f563
jspvztiiiiiiiii <1825121802@qq.com>
2026-04-12 09:35:56 +0800

    Update IZYZ.js

--- level/IZYZ.js
+++ level/IZYZ.js
@@ -11,9 +11,14 @@ var oINewspaperZombie=InheritO(oNewspaperZombie,{
     EName:"oINewspaperZombie",
     SunNum:125
 });
+var oIPeaZombie=InheritO(oPeaZombie,{
+    EName:"oINewspaperZombie",
+    SunNum:75,
+	coolTime:7.5,
+});
 oS.Init({
     PName: [oIZombie, oBucketheadZombie, oPeaZombie,oImp],
-    ZName: [oZombie, oINewspaperZombie,oPeaZombie,oConeheadZombie,oImp],
+    ZName: [oZombie, oINewspaperZombie,oIPeaZombie,oConeheadZombie,oImp],
     PicArr: ["images/interface/background1.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
     backgroundImage: "images/interface/background1.jpg",
     ShowScroll: false,
