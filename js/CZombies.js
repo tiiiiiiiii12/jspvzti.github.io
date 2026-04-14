55a3b84ea2ea914a9d1be56705dc2ef7fcc49b8d
jspvztiiiiiiiii <1825121802@qq.com>
2026-04-14 22:12:18 +0800

    Update CZombies.js

--- js/CZombies.js
+++ js/CZombies.js
@@ -3379,7 +3379,7 @@ oJackinTheBoxZombie = InheritO(OrnNoneZombies, {
 jinyinAct:function(a){
 	a.Act=function(a){
 		var z=oZ.getZ0(a.ZX,a.R);
-		!(a.pushZ&&z.id=a.id)?(a.ZX>=400&&(a.pushZ=z,SetStyle(z.EleBody, {
+		!(a.pushZ&&(z.id=a.id))?(a.ZX>=400&&(a.pushZ=z,SetStyle(z.EleBody, {
                     top: (z.height*0.9)+"px",
                     clip: "rect(0,auto,"+(z.height*0.1)+"px,0)"
                 }),z.Altitude=4,
