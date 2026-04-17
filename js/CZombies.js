b3aa5f3d96a4b6c833d57b89dff23e883a6df592
jspvztiiiiiiiii <1825121802@qq.com>
2026-04-18 00:21:20 +0800

    Update CZombies.js

--- js/CZombies.js
+++ js/CZombies.js
@@ -3376,16 +3376,16 @@ oJackinTheBoxZombie = InheritO(OrnNoneZombies, {
     ];
   })(),
 GoingDieHead:function(){},
-  AudioArr: ["zombie_entering_water"],
+  AudioArr: ["wakeup"],
 jinyinAct:function(a){
 	a.Act=function(a){
 		var z=oZ.getZ0(a.ZX,a.R);
-		z&&z.EName!=a.EName&&(!a.pushZ&&(z.Altitude==1)?(a.ZX>=400&&(a.pushZ=z,SetStyle(z.EleBody, {
+		z&&z.EName!=a.EName&&(!a.pushZ?(a.ZX>=400&&(a.pushZ=z,SetStyle(z.EleBody, {
                     top: (z.height*0.7)+"px",
                     clip: "rect(0,auto,"+(z.height*0.3)+"px,0)"
                 }),z.Altitude=4,z.isAttacking=0,
 			z.FreeSetbodyTime=1,SetHidden(z.EleShadow))):(
-			a.ZX>=400&&a.pushZ.HP?a.pushZ.getr(a.pushZ,-a.Speed,1):(a.pushZ.HP&&(SetStyle(a.pushZ.EleBody, {
+			a.ZX>=400&&a.pushZ&&a.pushZ.HP?a.pushZ.getr(a.pushZ,-a.Speed,1):(a.pushZ.HP&&(SetStyle(a.pushZ.EleBody, {
                     top: "0px",
                     clip: "rect(0,auto,"+a.pushZ.height+"px,0)"
                 }),
