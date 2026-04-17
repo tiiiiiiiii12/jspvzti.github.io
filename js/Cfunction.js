731191d79d6e771ea829ff5c5fe049a48cd3da78
jspvztiiiiiiiii <1825121802@qq.com>
2026-04-17 22:39:02 +0800

    Update Cfunction.js

--- js/Cfunction.js
+++ js/Cfunction.js
@@ -1500,7 +1500,7 @@ InitHandBookPCard = function() {
 InitHandBookZCard = function() {
 	PlayAudio("gravebutton");
 	var d = "",
-	g, f, e = [oZombie, oFlagZombie, oConeheadZombie, oPoleVaultingZombie, oBucketheadZombie, oNewspaperZombie,oScreenDoorZombie,oFootballZombie,oDancingZombie,oBackupDancer,oDuckyTubeZombie1,oSnorkelZombie,oZomboni,oDolphinRiderZombie,oImp,oJackinTheBoxZombie,oBalloonZombie,oGargantuar],
+	g, f, e = [oZombie, oFlagZombie, oConeheadZombie, oPoleVaultingZombie, oBucketheadZombie, oNewspaperZombie,oScreenDoorZombie,oFootballZombie,oDancingZombie,oBackupDancer,oDuckyTubeZombie1,oSnorkelZombie,oZomboni,oDolphinRiderZombie,oImp,oJackinTheBoxZombie,oBalloonZombie,oDiggerZombie,oGargantuar,oPeaZombie,oGatlingPeaZombie],
 	a = e.length,
 	b = 0,
 	c;
