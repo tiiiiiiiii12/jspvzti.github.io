d73adf7c4d57e43e075e117fab9a907d198083ca
jspvztiiiiiiiii <1825121802@qq.com>
2026-04-18 18:07:56 +0800

    Enhance Produce description for Newspaper Zombie
    Updated the Produce property to include additional details about the zombie's abilities and behavior.

--- js/NewZombies.js
+++ js/NewZombies.js
@@ -319,7 +319,7 @@ PrivateDie:function(a){
             return ["images/Card/Zombies/NewspaperZombie.png", a + "0.gif", a + "HeadWalk1.gif", a + "HeadAttack1.gif", a + "LostHeadWalk1.gif", a + "LostHeadAttack1.gif", a + "HeadWalk0.gif", a + "HeadAttack0.gif", a + "LostHeadWalk0.gif", a + "LostHeadAttack0.gif", a + "Head.gif" + $Random, a + "Die.gif" + $Random, a + "BoomDie.gif" + $Random, a + "LostPaper.gif", a + "1.gif"]
         })(),
         AudioArr: ["newspaper_rarrgh2"],
-        Produce: '他的报纸只能提供有限的防御。<p>韧性：<font color="#FF0000">中（450，发怒后50%减伤）</font><br>报纸韧性：<font color="#FF0000">低</font><br>速度：正常，而后快(失去报纸后)</font><br>伤害：正常，而后4倍(失去报纸后)</p>读报僵尸，他正痴迷于完成他的数独难题。难怪他这么反常。',
+        Produce: '他的报纸只能提供有限的防御。<p>韧性：<font color="#FF0000">中（450，发怒后50%减伤）</font><br>报纸韧性：<font color="#FF0000">低</font><br>速度：正常，而后快(失去报纸后)</font><br>伤害：正常，而后4倍(失去报纸后)<br>精英形态：<font color="#FF0000">发怒后，每隔4秒召唤双倍攻速的豌豆僵尸</font></p>读报僵尸，他正痴迷于完成他的数独难题。难怪他这么反常。',
 		jinyinAct:function(a){},
 		bedevil:oPeaZombie.prototype.bedevil,
 		  shootPea: function() {
