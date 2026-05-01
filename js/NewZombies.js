1e3bdcdfa8846b9bdb9c35dfe0c359f2c10a49c3
jspvztiiiiiiiii <1825121802@qq.com>
2026-05-01 22:30:39 +0800

    Update NewZombies.js

--- js/NewZombies.js
+++ js/NewZombies.js
@@ -172,7 +172,7 @@ var oGargantuar = InheritO(oZombie, {
       !h.hasthrew&&!(h.PZ == h.check) && (
         EditImg($(P.FumeDoor), 0,"images/interface/target.png", {
           transform: h.PZ ? "rotateY(180deg)" : "rotateY(0deg)",
-          left: h.PZ ? "100px" : "-80px"
+          left: h.PZ ? "125px" : "-50px"
         }, 0),
         h.check = h.PZ);
     }
@@ -182,7 +182,7 @@ var oGargantuar = InheritO(oZombie, {
     var z = $(a.id);
 	a.num=Math.round(Math.random()*1+0)||a.Privatenum;
     z.FumeDoor = "Fume" + Math.random();
-    var Sh = NewImg(z.FumeDoor, "images/interface/target.png", "position:absolute;transform:" + (a.PZ ? "rotateY(180deg);" : "rotateY(0deg);") + "left:100px;top:"+(a.num?"40px;":"-80px;"), 0);
+    var Sh = NewImg(z.FumeDoor, "images/interface/target.png", "position:absolute;transform:" + (a.PZ ? "rotateY(0deg);" : "rotateY(180deg);") + "left:125px;top:150px"), 0);
     z.appendChild(Sh);
 	oSym.addTask(750,function(b){
 		$Z[b.id]&&b.hasthrew < b.throwImpnum &&(oP.SetTimeoutAirdropZombie(5, 9, 1, [oImp, oZombie, oConeheadZombie, oPeaZombie, oScreenDoorZombie, oZombie2, oZombie3, oBucketheadZombie,oNewspaperZombie,oJackinTheBoxZombie,oPoleVaultingZombie], !b.PZ),
