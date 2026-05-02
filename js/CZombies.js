a7f96dc25e55f0a7a317c7e68fdf51076d4bb3b9
jspvztiiiiiiiii <1825121802@qq.com>
2026-05-02 09:04:01 +0800

    Update CZombies.js

--- js/CZombies.js
+++ js/CZombies.js
@@ -1746,7 +1746,7 @@ c.JudgeAttack = c.JudgeAttackH;
                 g.ChkActs1 = function() {
                     return 1
                 },
-                g.EleBody.src = f[g.LostPaperGif] + $Random + Math.random(), g.Ornaments = 0, g.LostHeadGif = 8, g.LostHeadAttackGif = 9, g.getFirePea = e.getFirePea, g.getSnowPea = e.getSnowPea, g.getFreezePea = e.getFreezePea,g.getHit = g.getHit0 = g.getHit1 = g.getHit2 = g.getHit3 = e.getHit, oSym.addTask(g.jinyin?450:150,
+                g.EleBody.src = f[g.LostPaperGif] + $Random + Math.random(), g.Ornaments = 0, g.LostHeadGif = 8, g.LostHeadAttackGif = 9, g.getFirePea = e.getFirePea, g.getSnowPea = e.getSnowPea, g.getFreezePea = e.getFreezePea,g.getHit = g.getHit0 = g.getHit1 = g.getHit2 = g.getHit3 = e.getHit, oSym.addTask(g.num?450:150,
                     function(m, l) {
                         var k = $Z[m];
                         if (!k) {
