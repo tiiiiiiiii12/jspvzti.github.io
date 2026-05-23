ee3e963233c3176ad11d06068daca0f170ca1027
jspvztiiiiiiiii <1825121802@qq.com>
2026-05-23 16:07:26 +0800

    Update ImZombie4.js

--- level/ImZombie4.js
+++ level/ImZombie4.js
@@ -2,7 +2,7 @@ var ojinyinJackinTheBoxZombie=InheritO(oJackinTheBoxZombie,{
     EName:"ojinyinJackinTheBoxZombie",
     CName:"精英玩偶匣僵尸",
     jinyinnum:100,
-    SunNum:150
+    SunNum:200
 });
 oS.Init({
     PName: [oThreepeater, oSunFlower, oSnowPea, oSpikeweed, oStarfruit, oGatlingPea, oTorchwood, oRepeater, oPotatoMine, oWallNut, oSpikerock, oGarlic, oCactus, oChomper, oSquash, oScaredyShroom, oTallNut],
