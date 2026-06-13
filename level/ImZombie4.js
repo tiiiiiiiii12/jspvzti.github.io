2285f9301cd4250f102f1ea5cb693416b486f1a6
jspvztiiiiiiiii <1825121802@qq.com>
2026-06-13 23:04:28 +0800

    Update ImZombie4.js

--- level/ImZombie4.js
+++ level/ImZombie4.js
@@ -5,7 +5,7 @@ var ojinyinJackinTheBoxZombie=InheritO(oJackinTheBoxZombie,{
     SunNum:200
 });
 oS.Init({
-    PName: [oThreepeater, oSunFlower, oSnowPea, oSpikeweed, oStarfruit, oGatlingPea, oTorchwood, oRepeater, oPotatoMine, oWallNut, oSpikerock, oGarlic, oCactus, oChomper, oSquash, oScaredyShroom, oTallNut],
+    PName: [oThreepeater, oSunFlower, oSnowPea, oSpikeweed, oStarfruit, oPuffShroom, oTorchwood, oRepeater, oPotatoMine, oWallNut, oSpikerock, oGarlic, oCactus, oChomper, oSquash, oScaredyShroom, oTallNut],
     ZName: [oScreenDoorZombie, oZombie3, oFlagZombie, oZomboni, oConeheadZombie, oFootballZombie, oPeaZombie, oPoleVaultingZombie,ojinyinJackinTheBoxZombie, oDiggerZombie, oBucketheadZombie],
     PicArr: ["images/interface/background2.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
     backgroundImage: "images/interface/background2.jpg",
@@ -24,7 +24,7 @@ oS.Init({
         ArC: [1, 6],
         ArR: [1, 5],
         Auto: 1,
-        P: [0, 1, 1, 1, 3, 3, 4, 5, 6, 6, 7, 8, 1, 9, 8, 9, 10, 11, 12, 12, 13, 1, 13, 14, 15, 15, 16, 2, 3, 7]
+        P: [0, 1, 1, 1, 3, 3, 4, 5, 6, 6, 7, 8, 1, 9, 8, 9, 10, 11, 12, 12, 13, 1, 13, 14, 15, 15, 16, 2, 2, 7]
     },
     RiddleAutoGrow: function() {
         var k = oS.ArP,
