f18a7f40bcc0acdeb3af19a6c0f897cc74e0f67b
jspvztiiiiiiiii <1825121802@qq.com>
2026-06-21 21:07:00 +0800

    Update ImZombiePool1.js

--- level/ImZombiePool1.js
+++ level/ImZombiePool1.js
@@ -1,6 +1,6 @@
 oS.Init({
     PName: [oFumeShroom, oPumpkinHead, oHypnoShroom, oPotatoMine, oSunFlower, oStarfruit, oScaredyShroom, oCactus, oPeashooter, oSnowPea, oGloomShroom, oGarlic, oPlantern,oSplitPea,oWallNut,oChomper,oRepeater],
-    ZName: [oZombie, oNewspaperZombie, oDuckyTubeZombie2, oDuckyTubeZombie3, oDolphinRiderZombie,oPoleVaultingZombie,oGargantuar,oSnorkelZombie,oLadderZombie],
+    ZName: [oZombie, oWallNutZombie, oDuckyTubeZombie2, oDuckyTubeZombie3, oDolphinRiderZombie,oPoleVaultingZombie,oGargantuar,oSnorkelZombie],
     PicArr: ["images/interface/background4.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"],
     backgroundImage: "images/interface/background4.jpg",
     Coord: 2,
