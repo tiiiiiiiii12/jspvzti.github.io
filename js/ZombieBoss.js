ccc22169d239e575a90a96b9c721992c38a647ca
jspvztiiiiiiiii <1825121802@qq.com>
2026-07-05 00:00:19 +0800

    Change HP threshold for ZombieBoss from 60000 to 50000

--- js/ZombieBoss.js
+++ js/ZombieBoss.js
@@ -52,7 +52,7 @@ var oGargantuarBoss = InheritO(oGargantuar, {
       oSym.addTask(500, function(a) {
 		a.getr(a,5,1);
         a.ChangeR(a);
-        if (a.HP >= 60000) {
+        if (a.HP >= 50000) {
 			try{
           oP.SetTimeoutZombie([oZombie, oZombie2, oZombie3], 0);
 		  oP.SetTimeoutTomZombie([oZombie]);
