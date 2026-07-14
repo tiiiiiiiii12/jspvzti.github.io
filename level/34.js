dd19c87f0a23c674f302747bd9d6ba97ec877e9a
jspvztiiiiiiiii <1825121802@qq.com>
2026-07-14 23:36:31 +0800

    Update zombie types and level configuration

--- level/34.js
+++ level/34.js
@@ -1,6 +1,6 @@
 oS.Init({
 	PName: [oPeashooter, oSunFlower, oCherryBomb, oWallNut, oPotatoMine, oSnowPea, oChomper, oRepeater, oPuffShroom, oSunShroom, oFumeShroom, oGraveBuster, oHypnoShroom, oScaredyShroom, oIceShroom, oDoomShroom, oLilyPad, oSquash, oThreepeater, oTangleKelp, oJalapeno, oSpikeweed, oTorchwood, oTallNut, oSeaShroom, oPlantern, oCactus, oBlover],
-	ZName: [oZombie, oZombie2, oDuckyTubeZombie1, oDuckyTubeZombie2, oDuckyTubeZombie3, oConeheadZombie, oBalloonZombie, oDolphinRiderZombie,oPeaZombie,oFootballZombie,oWallNutZombie],
+	ZName: [oZombie, oZombie2, oDuckyTubeZombie1, oDuckyTubeZombie2, oDuckyTubeZombie3, oConeheadZombie, oBalloonZombie, oDolphinRiderZombie,oPeaZombie,oTallNutZombie],
 	PicArr: ["images/interface/background4.jpg","images/interface/Taco.png"],
 	Coord: 2,
 	SunNum: 150,
@@ -21,7 +21,7 @@ oS.Init({
 	StartGameMusic: "Loonboon"
 },
 {
-	AZ: [[oZombie, 2, 1], [oZombie2, 2, 1], [oDuckyTubeZombie1, 1, 6, [6, 7, 8, 10, 19, 20]], [oDuckyTubeZombie2, 1, 6, [10, 20]], [oDuckyTubeZombie3, 1, 6, [10, 20]],[oConeheadZombie, 3, 1],[oBalloonZombie, 1, 10,[10,20]], [oDolphinRiderZombie, 1, 10, [10, 20]],[oPeaZombie,1,1],[oFootballZombie,1,1],[oWallnutZombie,1,1]],
+	AZ: [[oZombie, 2, 1], [oZombie2, 2, 1], [oDuckyTubeZombie1, 1, 6, [6, 7, 8, 10, 19, 20]], [oDuckyTubeZombie2, 1, 6, [10, 20]], [oDuckyTubeZombie3, 1, 6, [10, 20]],[oConeheadZombie, 3, 1],[oBalloonZombie, 1, 10,[10,20]], [oDolphinRiderZombie, 1, 10, [10, 20]],[oPeaZombie,1,1],[oTallNutZombie,1,1]],
 	FlagNum: 20,
 	FlagToSumNum: {
 		a1: [3, 5, 9, 10, 13, 15, 19],
