5c757110029bcbb971371839c0f44b1a0e7ef2ca
jspvztiiiiiiiii <1825121802@qq.com>
2026-07-14 23:58:34 +0800

    Update VaseKiller2.js

--- level/VaseKiller2.js
+++ level/VaseKiller2.js
@@ -1,7 +1,7 @@
 // VaseKiller2
 oS.Init({
 	PName: [oCherryBomb, oRepeater2, oSnowPea, oWallNut, oPotatoMine],
-	ZName: [oZombie, oZombie2, oZombie3, oBucketheadZombie, oJackinTheBoxZombie],
+	ZName: [oZombie, oZombie2, oZombie3, oBucketheadZombie, oJackinTheBoxZombie,oNewspaperZombie],
 	PicArr: ["images/interface/background2.jpg","images/interface/trophy.png","images/interface/PointerDown.gif","images/interface/Stripe.png"], 
 	backgroundImage: "images/interface/background2.jpg", 
 	DKind: 0, ShowScroll: false, ProduceSun: false, 
@@ -44,7 +44,7 @@ oS.Init({
 	}, 
 	VaseArP : {
 		GreenNum: 2, Left: 4, Right: 8, 
-		ZombieP: [0, 0, 1, 1, 2, 2, 3, 3, 3, 4], 
+		ZombieP: [0, 0, 1, 1, 2, 2, 3, 3, 4,5], 
 		PlantP: [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4], 
 		SunP: [] 
 	}
