fbf9983120b95edc55283bdadf7ab37f1cbdcbcf
jspvztiiiiiiiii <1825121802@qq.com>
2026-08-13 17:08:55 +0800

    Update CPlant.js

--- js/CPlant.js
+++ js/CPlant.js
@@ -2540,7 +2540,7 @@ for (let i = Math.max(1, f.R - 1); i <= Math.min(f.R + 1, oS.R); i++) {
 				PlayAudio("coffee");
 				for (i=1;i<=2;i++){
 					var P=oGd.$[a.R+"_"+a.C+"_"+i];
-					P&&(P.HP+=Math.min(P.MaxHP-P.HP,Math.max(200,P.MaxHP*0.2))
+					P&&(P.HP+=Math.min(P.MaxHP-P.HP,Math.max(200,P.MaxHP*0.2)))
 				}
 			oSym.addTask(500,arguments.callee,[a])
 			},[a])
