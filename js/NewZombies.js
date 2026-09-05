64d0c7f710df00e446cd57378b40ea990ace0506
jspvztiiiiiiiii <1825121802@qq.com>
2026-09-05 09:58:21 +0800

    Update NewZombies.js

--- js/NewZombies.js
+++ js/NewZombies.js
@@ -726,7 +726,7 @@ SetStyle($(z.JaHead),{
 		if(oGd.$Ladder[y+"_"+i]&&!this.PZ) delete oGd.$Ladder[y+"_"+i];
         for (let j = 0; j < 4; j++) {
           let g = oGd.$[y + "_" + i + "_" + j];
-          this.PZ&&g&&g.getHurt(this,3,1600*this.level)//精英小鬼辣椒僵尸释放技能
+          this.PZ&&g&&g.getHurt(this,3,1600*this.level)
       }
 	}
     },
@@ -947,7 +947,7 @@ oLadderZombie = InheritO(oScreenDoorZombie, {
   })(),
   jinyinAct: function(a) {
 	a.num = a.Privatenum||Math.random() * 100;
-	if(!a.num){
+	if(a.num<50){
     a.OSpeed /= 2;
     a.Speed /= 2;
 	a.ZKind=1;
@@ -1092,7 +1092,7 @@ jianshangtime:500,
     var a = $Z[c];
     a&&(a.EleBody.src = a.PicArr[a.LadGif]);
     oSym.addTask(50, function(a, b) {
-      a&&a.beAttacked&&($P[b]&&$P[b].canEat&&(a.Ornaments&&$P[b].getLadder(), !a.num&&(a.OrnHP=0,a.getHit0(a, 0, 0))),a.JudgeAttack());
+      a&&a.beAttacked&&($P[b]&&$P[b].canEat&&(a.Ornaments&&$P[b].getLadder(),(!a.jinyin||a.num<50)&&(a.OrnHP=0,a.getHit0(a, 0, 0))),a.JudgeAttack());
     }, [a, b])
   },
   canLadderList: {
@@ -1104,6 +1104,6 @@ jianshangtime:500,
   Produce: '他遇到防御植物可将梯子搭在上面<br>韧性：<font color="#FF0000">中</font><br>扶梯韧性：<font color="#FF0000">中(500)</font><br>精英形态一：<font color="#FF0000">扶梯→玉米加农炮，速度变慢，每隔一段时间朝随机一颗植物发射炮弹，对该植物的3*3范围造成1600伤害</font><br>精英形态二：<font color="#FF0000">金色扶梯，无限搭梯，自身有25%减伤，免疫减速、冻结、击退，每隔一段时间给周围僵尸附上25%减伤</font><br>这架梯子花了他$114514。',
   CheckOrnHP: function(g, h, d, c, f, b, a) {
     var e = OrnNoneZombies.prototype;
-    (g.OrnHP = d -= c) < 1 && (a && (g.HP += d), g.Ornaments = 0, g.EleBody.src = f[[g.NormalGif = g.OrnLostNormalGif, g.AttackGif = g.OrnLostAttackGif][b]], g.LostHeadGif = 8, g.LostHeadAttackGif = 9, g.getPea = e.getPea, g.getFreezePea = e.getFreezePea, g.getFirePea = e.getFirePea, g.getFirePeaSputtering = e.getFirePeaSputtering,g.OSpeed=g.LostPaperSpeed,g.Speed=g.LostPaperSpeed*(g.FreeSlowTime?0.5:1),!g.num&&(g.getSnowPea = e.getSnowPea), g.PlayNormalballAudio = e.PlayNormalballAudio, g.PlayFireballAudio = e.PlayFireballAudio, g.PlaySlowballAudio = e.PlaySlowballAudio, g.canLadderList = [], g.Boom = function() {}, g.getHit = g.getHit0 = g.getHit1 = g.getHit2 = g.getHit3 = e.getHit)
+    (g.OrnHP = d -= c) < 1 && (a && (g.HP += d), g.Ornaments = 0, g.EleBody.src = f[[g.NormalGif = g.OrnLostNormalGif, g.AttackGif = g.OrnLostAttackGif][b]], g.LostHeadGif = 8, g.LostHeadAttackGif = 9, g.getPea = e.getPea, g.getFreezePea = e.getFreezePea, g.getFirePea = e.getFirePea, g.getFirePeaSputtering = e.getFirePeaSputtering,g.OSpeed=g.LostPaperSpeed,g.Speed=g.LostPaperSpeed*(g.FreeSlowTime?0.5:1),(!g.jinyin||g.num<50)&&(g.getSnowPea = e.getSnowPea), g.PlayNormalballAudio = e.PlayNormalballAudio, g.PlayFireballAudio = e.PlayFireballAudio, g.PlaySlowballAudio = e.PlaySlowballAudio, g.canLadderList = [], g.Boom = function() {}, g.getHit = g.getHit0 = g.getHit1 = g.getHit2 = g.getHit3 = e.getHit)
   }
 })
