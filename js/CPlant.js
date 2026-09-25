48d217baaa5fb659c928aeaa3fc43bdbb9e55ede
jspvztiiiiiiiii <1825121802@qq.com>
2026-09-25 10:53:18 +0800

    Update CPlant.js

--- js/CPlant.js
+++ js/CPlant.js
@@ -3643,7 +3643,7 @@ NormalAttack2: function() {
         SunNum: 100,
         PicArr: ["images/Card/Plants/Blover.png", "images/Plants/Blover/0.gif", "images/Plants/Blover/Blover.gif"],
         Tooltip: "能吹走所有气球和迷雾",
-        Produce: '三叶草，能吹走所有的气球僵尸，也可以把雾吹散，把僵尸吹退几格<p>使用方法：<font color="#FF0000">单独使用，立即生效</font><br>特点：<font color="#FF0000">吹走屏幕上所有的气球僵尸</font></p>当三叶草五岁生日的时候，他得到了一个闪亮的生日蛋糕。他许好愿，然后深吸一口气却只吹灭了60%的蜡烛。然而他没有放弃，小时候的那次失败促使他更加努力直到现在。',
+        Produce: '三叶草，能吹走所有的气球僵尸，也可以把雾吹散，把僵尸吹退几格<br>使用方法：<font color="#FF0000">单独使用，立即生效</font><br>精英形态：<font color="#FF0000">对全屏僵尸生效一次磁力菇的效果</font><br>特点：<font color="#FF0000">吹走屏幕上所有的气球僵尸</font><br>当三叶草五岁生日的时候，他得到了一个闪亮的生日蛋糕。他许好愿，然后深吸一口气却只吹灭了60%的蜡烛。然而他没有放弃，小时候的那次失败促使他更加努力直到现在。',
         AudioArr: ['blover'],
         InitTrigger: function() {},
         PrivateBirth: function(o) { // 种植后0.5秒开始吹风
@@ -3655,7 +3655,7 @@ NormalAttack2: function() {
             var id = this.id,
                 z, oBalloon;
             $(id).childNodes[1].src = 'images/Plants/Blover/Blover.gif';
-            for (z in $Z) oBalloon = $Z[z],oBalloon&& oBalloon.getr(oBalloon,160),oBalloon&&this.jinyin&& oBalloon.getHit2(oBalloon,300), (oBalloon.EName == 'oBalloonZombie') && oBalloon.getDispelled(); //把气球吹跑
+            for (z in $Z) oBalloon = $Z[z],oBalloon&& oBalloon.getr(oBalloon,160),this.jinyin&&oBalloon&&((oBalloon.EName == 'oDiggerZombie')&&oBalloon.PZ&&oBalloon.Stone_of_Sinan_Up(),(oBalloon.EName == 'oJackinTheBoxZombie')&&oBalloon.PZ&&(oBalloon.OpenBox=function(){}),oBalloon.OrnHP=0,oBalloon.getHit0(oBalloon,0,0)), (oBalloon.EName == 'oBalloonZombie') && oBalloon.getDispelled(); //把气球吹跑
             if (oS.HaveFog) { // 如果场地上有雾，驱散
                 oGd.MoveFogRight(); // 驱散雾
                 oSym.addTask(2400 + 150, oGd.MoveFogLeft, []); // 24s后恢复
