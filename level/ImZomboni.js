0d40f5aa1f2cc3858ebe84b648d7fc3e16278ae7
jspvztiiiiiiiii <1825121802@qq.com>
2026-09-26 01:30:04 +0800

    Add new plants and update zombie spawn points

--- level/ImZomboni.js
+++ level/ImZomboni.js
@@ -13,7 +13,7 @@ var oJalapenoZomboni=InheritO(oZomboni,{
     SunNum:225
 });
 oS.Init({
-    PName: [oSunFlower,oRepeater,oGatlingPea,oChomper,oFumeShroom,oTallNut,oBlover,oPeashooter,oThreepeater], // 植物名称数组
+    PName: [oSunFlower,oRepeater,oGatlingPea,oChomper,oFumeShroom,oTallNut,oBlover,oPeashooter,oThreepeater,oSpikerock,oPotatoMine,oSquash], // 植物名称数组
     ZName: [oJalapenoZomboni,oIceShroomZomboni], // 僵尸名称数组
     PicArr: ["images/interface/background2.jpg", "images/interface/trophy.png", "images/interface/Stripe.png"], // 图片数组
     backgroundImage: "images/interface/background2.jpg", // 背景图片
@@ -44,7 +44,19 @@ oS.Init({
 		[1, 3, 2],
 		[7, 4, 2],
     [1 ,5, 2],
-	  [8, 6, 2]
+	  [8, 6, 2],
+	  [9, 2, 3],
+	  [5, 2, 4],
+	  [1, 1, 4],
+	  [4, 3, 4],
+	  [10, 6, 4],
+	  [0, 5, 4],
+	  [0,4,4],
+	  [1,3,3],
+	  [4,1,3],
+	  [3,6,3],
+	  [10, 5, 3],
+	  [11, 4, 3]
 	] // P数组
     },
     // 自动增长谜题
