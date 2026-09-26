533137d7dec18d6cf545b3341ba926e665ac70b6
jspvztiiiiiiiii <1825121802@qq.com>
2026-09-26 17:04:02 +0800

    Update NewZombies.js

--- js/NewZombies.js
+++ js/NewZombies.js
@@ -172,11 +172,11 @@ var oGargantuar = InheritO(oZombie, {
     }, SetVisible(h.EleShadow)), $(h.Ele.FumeDoor) && ($(h.Ele.FumeDoor).style.top = "80px"), h.intowater = false);
     if (h.jinyin) {
       var P = $(h.id);
-      !h.hasthrew && (h.WalkDirection == h.check) && (
-        EditImg($(P.FumeDoor), 0, h.num >= 50 ? "images/interface/target.png" : "images/Plants/Blover/Blover.gif", {
+       $(P.FumeDoor)&& (h.WalkDirection == h.check) && (
+        SetStyle($(P.FumeDoor), {
           transform: !h.WalkDirection ? "rotateY(0deg)" : "rotateY(180deg)",
           left: !h.WalkDirection ? "200px" : "20px"
-        }, 0),
+        }),
         h.check = h.WalkDirection ? 0 : 1);
     }
     h.canWalk(h, h.id) && h.hasthrew < h.throwImpnum && (GetC(h.ZX) > 3 || !h.PZ) && !h.isAttacking && (h.HP <= h.MaxHP * 0.5) && h.throwImp(h);
@@ -264,7 +264,7 @@ var oGargantuar = InheritO(oZombie, {
   SunNum: 275,
   EName: "oGargantuar",
   CName: "伽刚特尔",
-  Produce: '非常强力的僵尸<br>韧性：<font color="#FF0000">极高(3000)</font><br>特点：<font color="#FF0000">半血丢小鬼，砸击植物，免疫击退</font><br>精英形态一：<font color="#FF0000">背着标靶，扔小鬼前每10秒空降一只僵尸，扔小鬼时召唤五个空降僵尸</font><br>精英形态二：<font color="#FF0000">每隔一段时间跳到随机位置</font><br>伽刚特尔的气场，是任何僵尸都无法比拟的，他是僵尸世界公认的偶像，他是最成功之僵。只是他出道十几年以来一直有个老大难的问题：他还是没有女朋友！'
+  Produce: '非常强力的僵尸<br>韧性：<font color="#FF0000">极高(3000)</font><br>特点：<font color="#FF0000">半血丢小鬼，砸击植物，免疫击退</font><br>精英形态一：<font color="#FF0000">背着标靶，扔小鬼前每10秒空降一只僵尸，扔小鬼时召唤五个空降僵尸</font><br>精英形态二：<font color="#FF0000">每隔一段时间跳到随机位置，丢小鬼后会加速</font><br>伽刚特尔的气场，是任何僵尸都无法比拟的，他是僵尸世界公认的偶像，他是最成功之僵。只是他出道十几年以来一直有个老大难的问题：他还是没有女朋友！'
 }),
 oPeaZombie = InheritO(oZombie, {
   EName: "oPeaZombie",
